3feac8e2c64e9a073e4271260e6a9711d8b7baf0
Maximilian Rahn <max.rahn@tum.de>
2020-03-29 19:52:25 +0200

    bit reset performed

--- src/components/Chart/Line/LineChart.vue
+++ src/components/Chart/Line/LineChart.vue
@@ -1,6 +1,6 @@
 <script>
 import LineChartCore from "./linechart-core.js";
-import ChartWrapper from '@/components/Chart/Wrapper/ChartWrapper.vue'
+import ChartWrapper from '@bit/maxthemillion.d3charts.c-wrapper'
 
 export default {
   name: "LineChart",
